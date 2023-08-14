70f0b6283318ecf813b6f56e8e4d3d0782e0e8af
capybare53 <bayraa093@gmail.com>
2023-08-14 05:37:29 +0300

    home page ui fix

--- user/urls.py
+++ user/urls.py
@@ -9,7 +9,13 @@ urlpatterns = [
   path('forgot', views.forgotPass, name='forgot'),
 
   path('profile/<str:pk>/', views.profilePath, name='profile'),
+
   path('profile-update', views.profileUpdate, name='profile-update'),
+  path('profile-update/points', views.profileUpdate, name='profile-update-points'),
+  path('profile-update/account', views.profileUpdate, name='profile-update-account'),
+  path('profile-update/notifications', views.profileUpdate, name='profile-update-notif'),
+  path('profile-update/pricing', views.profileUpdate, name='profile-update-pricing'),
+
   path('profile-result/<str:pk>/', views.profileResult, name='profile-result'),
   path('delete', views.deleteUser, name='delete'), 
 
